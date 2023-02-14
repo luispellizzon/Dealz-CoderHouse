530c5c69717a4177420c52b8ee5dc3e385558e9f
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-02-14 20:09:56 +0000

    Adding Ver Detalhes Button on each item image

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,8 +4,12 @@ const Item = ({ item, addOnCart }) => {
 
   return (
     <div className="rounded flex flex-col h-80" id={id}>
-      <div className="w-full min-h-[70%]">
-        <img src={imageUrl} alt="" className="w-full h-full rounded-t" />
+      <div className="w-full min-h-[70%] relative group">
+        <img src={imageUrl} alt="" className="w-full h-full rounded-t  " />
+        <div className="hidden w-full h-full absolute top-0 left-0 group-hover:flex items-center justify-center group-hover:bg-black group-hover:bg-opacity-60">
+          <button type="button"
+          className="w-[60%] bg-amber-50 px-2 py-1 rounded text-slate-900 font-bold active:scale-95 cursor-pointer">Ver Detalhes</button>     
+        </div>
       </div>
       <div className="h-full flex flex-col justify-between text-center">
         <div className="pt-2">
