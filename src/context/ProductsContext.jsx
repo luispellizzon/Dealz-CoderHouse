3636a4b5181b1e45a00dbdb0655708fdfeb261f8
Luis Pellizzon <luis.pellizzon@gmail.com>
2023-03-09 12:25:07 +0000

    Creating Products Context

---
+++ src/context/ProductsContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+export const ProductsContext = createContext({
+  products: [],
+});
+
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const value = {
+    products,
+  };
+  return (
+    <ProductsContext.Provider value={value}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
